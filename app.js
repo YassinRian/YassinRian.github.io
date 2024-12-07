9f3aeeda6a01a7c8e910dd224fbc1afa08450bb2
Yassin Rian <yassinrian@hotmail.com>
2024-12-07 03:07:09 +0100

    khalid

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ define(function () {
 
 	BasicControl.prototype.draw = function (oControlHost) {
 		const cont = oControlHost.container;
-		cont.innerHTML = "<h1>great ... working!!</h1>";
+		cont.innerHTML = "<h1>is it still working!!</h1>";
 	};
 	return BasicControl;
 });
