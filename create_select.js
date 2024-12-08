e80bf2700636c91b6f3b02cba02654637817e89f
YassinRian <yassinrian@hotmail.com>
2024-12-08 20:27:14 +0100

    Commit made on 2024-12-08 20:27:14

--- create_select.js
+++ create_select.js
@@ -2,7 +2,7 @@ define(['jquery'], function ($) {
 
     function create_select () {};
 
-    create_select.prototype.drawDataStore = function(oDataStore) {
+    create_select.prototype.draw = function(oDataStore) {
                 console.log(oDataStore);
     }
   
