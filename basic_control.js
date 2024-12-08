d3b5683c8219ec2066f4dd865f2911f950eddfd2
YassinRian <yassinrian@hotmail.com>
2024-12-08 13:59:25 +0100

    Commit made on 2024-12-08 13:59:25

--- basic_control.js
+++ basic_control.js
@@ -5,7 +5,7 @@ define(function () {
 
     BasicControl.prototype.draw = function(oControlHost) {
         const cont = oControlHost.container;
-        cont.innerHTML = "<h1>nou weer terug bij af</h1>"
+        cont.innerHTML = "<h1>dit werkt natuurlijk beter</h1>"
     };
     return BasicControl;
 });
