0030d81ed91d1fffb189b3212d2af27b381d4e77
YassinRian <yassinrian@hotmail.com>
2024-12-08 20:19:21 +0100

    Commit made on 2024-12-08 20:19:21

--- create_select.js
+++ create_select.js
@@ -1,6 +1,9 @@
 define(['jquery'], function ($) {
-    return {
-        initialize: function (options) {
+
+    function create_select () {};
+
+    create_select.prototype.draw = function(options) {
+  
             var control = this;
             var data = options.data; // This contains the `cluster_naam` data
 
@@ -34,17 +37,5 @@ define(['jquery'], function ($) {
                 var selectedValue = $(this).val();
                 control.setValues([{ use: selectedValue, display: selectedValue }]);
             });
-        },
-
-        // Function to set values programmatically (if needed)
-        setValues: function (values) {
-            $('#' + this.options.controlId + ' select').val(values[0].use);
-        },
-
-        // Function to get the current values (if needed)
-        getValues: function () {
-            var selectedValue = $('#' + this.options.controlId + ' select').val();
-            return [{ use: selectedValue, display: selectedValue }];
         }
-    };
 });
