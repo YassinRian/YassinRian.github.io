783f57e067bd073e9c9d66d8b868986d4f4e3c30
YassinRian <yassinrian@hotmail.com>
2024-12-10 22:39:17 +0100

    Commit made on 2024-12-10 22:39:17

--- basic_control.js
+++ basic_control.js
@@ -16,7 +16,7 @@ define(function () {
                 return "123";
             }
         }
-        let obj_yas = new_b51(someObject);
+        let obj_yas = new _b51(someObject);
         let uniqueId = obj_yas.generateUniqueID();
         console.log(uniqueId);
         // console.log(oControlHost.prototype.getParameter())
