4b6ff2eb8bde8ffbb5f95434221a3fa859ed5193
YassinRian <yassinrian@hotmail.com>
2024-12-10 23:01:14 +0100

    Commit made on 2024-12-10 23:01:14

--- basic_control.js
+++ basic_control.js
@@ -7,7 +7,8 @@ define(function () {
         let { parameter } = oControlHost.configuration;
         console.log(oControlHost.configuration)
         console.log(oControlHost)
-
+        // hier heb ik een instance van _b51 met een eigen object waarden die ik meegeef :)
+        // _b51 is oControlHost (eigenlijk de instance van _b51)
         let someObject = {
             _05d: function() {
                 return "some value";
@@ -18,10 +19,19 @@ define(function () {
         }
         let obj_yas = new _b51(someObject);
         let uniqueId = obj_yas.generateUniqueID();
+        let parameter_ = obj_yas.getParameter("p_artikel_beschrijving")
         console.log(uniqueId);
+        console.log(parameter_);
         // console.log(oControlHost.prototype.getParameter())
         //console.log(oControlHost.xmlElement)
         
     };
+
+
+
+
+
+
+
     return BasicControl;
 });
