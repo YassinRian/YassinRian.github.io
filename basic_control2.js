cbabd445aeb0c3c4a43a476ff37a52f0adbc5d1a
YassinRian <yassinrian@hotmail.com>
2024-12-11 21:22:30 +0100

    Commit made on 2024-12-11 21:22:30

--- basic_control2.js
+++ basic_control2.js
@@ -26,7 +26,7 @@ define(['jquery'], function ($) {
     //         oControlHost.page.getControlByName("List1").toggleVisibility();
     //     })
     BasicControl.prototype.draw = function(oPage){
-       console.log(oPage.page.GetReportXml());
+       console.log(oPage.page.application.GetReportXml());
     };
 
     return BasicControl;
