9992dc504290190bee0069f7686a7fd93774c99a
YassinRian <yassinrian@hotmail.com>
2024-12-11 13:27:38 +0100

    Commit made on 2024-12-11 13:27:38

--- basic_control2.js
+++ basic_control2.js
@@ -4,7 +4,20 @@ define(function () {
     function BasicControl() { };
 
     BasicControl.prototype.draw = function(oControlHost){
-        console.log(oControlHost.getParameter("p_artikel_beschrijving"));
+        let _1wh = 'some_value_for_s1h';
+        let _cvn = 'some_name';
+        let _97n = 123
+        let _psn = {
+            columns: [
+                {name: 'Column1', values: [1, 2, 3], formattedValues: ['1', '2', '3'], dataType: 'decimal'},
+                {name: 'Column2', values: [4, 5, 6], formattedValues: ['4', '5', '6'], dataType: 'string'}
+            ],
+            rows: [0, 1, 2]
+        };
+        let obj_yassin = new _3a5(_1wh, _cvn, _97n, _psn);
+        console.log(obj.name);
+        console.log(obj.rowCount);
+        console.log(obj.columnNames);
     }
 
     return BasicControl;
