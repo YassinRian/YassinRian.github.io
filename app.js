f2b729495fe0338a1ec4727587ba0a9458cd377c
YassinRian <yassinrian@hotmail.com>
2024-12-12 14:10:10 +0100

    Commit made on 2024-12-12 14:10:10

--- app.js
+++ app.js
@@ -27,16 +27,8 @@ function filter_lijst(_this_) {
 
 App.prototype.draw = function(oControlHost) {
 
-    let _1wh = 'some_value_for_s1h'; //some dummy value -- wat deze waarden dienen te zijn weet ik niet
-    let _cvn = 'some_name'; // dummy value
-    let _97n = 123 // dummy value
-    let _psn = this.DataStore._x3c._9tg[0]; // dit werkt !! wow:) -- dit is dat die meegegeven wordt om een instantie van _3a5 te kunnen maken 
-    
-    
-    let obj_yassin = new _3a5(_1wh, _cvn, _97n, _psn); // nieuwe instantie van _3a5, dit is een object met handige functies
-
     let elm = oControlHost.container;
-	$(elm).append(html_func_.html(obj_yassin.json.columns)); // voeg html aan component
+	$(elm).append(html_func_.html(this.DataStore.json.columns)); // voeg html aan component
 
 // input velden referen naar een selectie box, hier wordt de link gelegd tussen input en selectie_box
 $('#box1').data({ select_class: 'select_1' })
