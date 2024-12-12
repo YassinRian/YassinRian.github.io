df1560fb1e3db471ded397715b62eac757bb0f8f
YassinRian <yassinrian@hotmail.com>
2024-12-12 18:31:22 +0100

    Commit made on 2024-12-12 18:31:22

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ App.prototype.draw = function(oControlHost) {
 
     let elm = oControlHost.container;
 	//$(elm).append(html_func_.html(basicControl.data)); // voeg html aan component
-   // console.log(this.data)
+   console.log(this.data, "vanuit draw App.js")
 
 // input velden referen naar een selectie box, hier wordt de link gelegd tussen input en selectie_box
 $('#box1').data({ select_class: 'select_1' })
