86a12cdf8aebbc5b765a264e934f934ef3d5f58e
YassinRian <yassinrian@hotmail.com>
2024-12-12 13:15:48 +0100

    Commit made on 2024-12-12 13:15:48

--- basic_control2.js
+++ basic_control2.js
@@ -4,6 +4,7 @@ define(['jquery'], function ($) {
     function BasicControl() { };
 
     BasicControl.prototype.draw = function(oControlHost){
+        
         let _1wh = 'some_value_for_s1h';
         let _cvn = 'some_name';
         let _97n = 123
@@ -22,8 +23,7 @@ define(['jquery'], function ($) {
         console.log(obj_yassin.name);
         console.log(obj_yassin.rowCount);
         console.log(obj_yassin.columnNames);
-        console.log(obj_yassin.json);
-        console.log(this.DataStore.rowCount);
+        console.log(obj_yassin.json); // dit werkt ook wow:)
         
 
     }
