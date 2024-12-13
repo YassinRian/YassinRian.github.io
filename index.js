60089b2efe2156a5612f67e2713075755f943caf
YassinRian <yassinrian@hotmail.com>
2024-12-13 16:00:10 +0100

    Commit made on 2024-12-13 16:00:10

--- index.js
+++ index.js
@@ -2,6 +2,59 @@ define(['jquery', 'https://yassinrian.github.io/html_func.js'], function($, html
 
     "use strict";
 
+                // input velden referen naar een selectie box, hier wordt de link gelegd tussen input en selectie_box
+                $('#box1').data({ select_class: 'select_1' });
+                $('#box2').data({ select_class: 'select_2' });
+    
+                // Set up event handlers
+                $('.wis_selecties').on('click', function() {
+                    let class_ = $(this).attr('data-selectie');
+                    $('.' + class_).find("option").map(function() {
+                        $(this).removeData();
+                        return this;
+                    }).prop('selected', false);
+                });
+    
+                $('input').on('keyup', function(e) {
+                    if (e.key === "Enter") {  // Only trigger on Enter key
+                        if ($(this).val().length > 1) {
+                            filter_lijst($(this));  // Call the filter_lijst function to filter the options
+                        } else {
+                            let selectie = $(this).data().select_class;
+                            let selec_vals = $("." + selectie).find("option");  // Get options
+                            selec_vals.map(function() {
+                                $(this).removeData();
+                                return this;
+                            }).prop('selected', false);
+                        }
+                    }
+                });
+
+    filter_lijst( _this_ )
+    {
+
+        let inp_val = $.trim(_this_.val().replace(/\s+/g, '').toUpperCase());
+        let selectie = _this_.data().select_class;
+        let selec_vals = $("." + selectie).find("option");
+    
+        // Compile the input value into a regular expression
+        let searchRegex = new RegExp(inp_val, 'i'); // 'i' for case-insensitive matching
+    
+        selec_vals.each(function () {
+            let optionText = $(this).text().replace(/\u00A0/g, '');
+            if (searchRegex.test(optionText)) {
+                $(this).data({ selected: true });
+            } else {
+                $(this).data({ selected: false });
+            }
+        });
+    
+        selec_vals.filter(function(){
+            return $(this).data().selected;
+            }).show().prop('selected', true);
+       
+    }
+
     class App
     {
 
@@ -18,32 +71,7 @@ define(['jquery', 'https://yassinrian.github.io/html_func.js'], function($, html
 
         draw(oControlHost)
         {
-
-            filter_lijst( _this_ )
-            {
-    
-                let inp_val = $.trim(_this_.val().replace(/\s+/g, '').toUpperCase());
-                let selectie = _this_.data().select_class;
-                let selec_vals = $("." + selectie).find("option");
-            
-                // Compile the input value into a regular expression
-                let searchRegex = new RegExp(inp_val, 'i'); // 'i' for case-insensitive matching
-            
-                selec_vals.each(function () {
-                    let optionText = $(this).text().replace(/\u00A0/g, '');
-                    if (searchRegex.test(optionText)) {
-                        $(this).data({ selected: true });
-                    } else {
-                        $(this).data({ selected: false });
-                    }
-                });
-            
-                selec_vals.filter(function(){
-                    return $(this).data().selected;
-                    }).show().prop('selected', true);
-               
-            }
-    
+ 
             let _1wh = 'some_value_for_s1h';
             let _cvn = 'some_name';
             let _97n = 123
@@ -55,35 +83,9 @@ define(['jquery', 'https://yassinrian.github.io/html_func.js'], function($, html
             let elm = oControlHost.container;
            $(elm).append(html_func_.html(obj_yassin.json.columns));
 
-            // input velden referen naar een selectie box, hier wordt de link gelegd tussen input en selectie_box
-            $('#box1').data({ select_class: 'select_1' });
-            $('#box2').data({ select_class: 'select_2' });
 
-            // Set up event handlers
-            $('.wis_selecties').on('click', function() {
-                let class_ = $(this).attr('data-selectie');
-                $('.' + class_).find("option").map(function() {
-                    $(this).removeData();
-                    return this;
-                }).prop('selected', false);
-            });
-
-            $('input').on('keyup', function(e) {
-                if (e.key === "Enter") {  // Only trigger on Enter key
-                    if ($(this).val().length > 1) {
-                        filter_lijst($(this));  // Call the filter_lijst function to filter the options
-                    } else {
-                        let selectie = $(this).data().select_class;
-                        let selec_vals = $("." + selectie).find("option");  // Get options
-                        selec_vals.map(function() {
-                            $(this).removeData();
-                            return this;
-                        }).prop('selected', false);
-                    }
-                }
-            });
-        }
-    }
+        } // draw
+    } // class
 
     return App;
 
