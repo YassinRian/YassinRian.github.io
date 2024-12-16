ce1f7232cabf4b15826e1e7b1dc7b57a661de90d
YassinRian <yassinrian@hotmail.com>
2024-12-16 13:56:37 +0100

    Commit made on 2024-12-16 13:56:37

--- control_app2.js
+++ control_app2.js
@@ -1,16 +1,16 @@
-define(['jquery', 'https://yassinrian.github.io/control_data2.js'], function($, data_){
+define(['jquery','https://yassinrian.github.io/control_data.js'], function($, data_){
 
     function App(){};
 
+
     App.prototype.draw = function(oControlHost) {
         const elm = oControlHost.container;
-        $(elm).append('<button id="button_yassin">Load Control</button>');
-        
-        $('#button_yassin').on('click', function() {
-            console.log("input click werkt !!");
-            let data_ins = new data_();
-            console.log(data_ins);           
-        })
+        $(elm).append('<button id="button_yassin">Hallo Yassin</button>');
+       //console.log(this.data);
+       $("#button_yassin").on('click', function(){
+        let data_inst = new data_();
+        console.log(data_inst.getData());
+       })
     }
     return App;
 }); // einde define
