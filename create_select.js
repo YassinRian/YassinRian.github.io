45824a5f7a02382a7f3a9e474f9ed3aa5249f693
YassinRian <yassinrian@hotmail.com>
2024-12-17 17:23:09 +0100

    Commit made on 2024-12-17 17:23:09

--- create_select.js
+++ create_select.js
@@ -1,9 +1,15 @@
-define(function () {
+define(['jquery'], function ($) {
 
     "use strict";
 
     function BasicControl () { };
 
-  alert("hallo yassin!!")
+    BasicControl.prototype.draw = function(oControlHost) {
+      let elm = oControlHost.container;
+      $(elm).append("<h1>Hallo Yassin</h1>")
+
+    }
+
+    return BasicControl;
   
 });
