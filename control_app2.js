9faeffbf241f8ee3d9717f43d93fe7ee7ba5f74e
YassinRian <yassinrian@hotmail.com>
2024-12-17 16:29:53 +0100

    Commit made on 2024-12-17 16:29:53

--- control_app2.js
+++ control_app2.js
@@ -5,11 +5,14 @@ define(['jquery'], function($){
     App.prototype.draw = function(oControlHost) {
         const elm = oControlHost.container;
         $(elm).append('<button id="button_yassin">Hallo Yassin</button>');
-        console.log(oControlHost.page.getControlByName("Control1").instance)
+        // console.log(oControlHost.page.getControlByName("Control1").instance)
        
-    //    $("#button_yassin").on('click', async function(){
+     $("#button_yassin").on('click', async function(){
+        console.log("geklikt");
+        let app_inst = new App();           
+        App.draw();
             
-    //    })
+      });
     }
 
     return App;
