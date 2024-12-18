9743a91f13397aa13c6344161e5ea16d168d5519
YassinRian <yassinrian@hotmail.com>
2024-12-18 12:07:10 +0100

    Commit made on 2024-12-18 12:07:10

--- create_select.js
+++ create_select.js
@@ -7,7 +7,7 @@ define(['jquery'], function ($) {
     BasicControl.prototype.draw = function(oControlHost) {
       let elm = oControlHost.container;
       $(elm).append("<h1>Hallo Yassin Rian!</h1>")
-      console.log(oControlHost.xmlElement)
+      console.log(oControlHost.xmlElement.textContent)
 
     }
 
