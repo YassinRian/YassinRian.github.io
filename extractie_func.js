bf05828d2d566e6506d14d4c293a530ff7aa11e5
YassinRian <yassinrian@hotmail.com>
2024-12-18 13:39:51 +0100

    Commit made on 2024-12-18 13:39:51

--- extractie_func.js
+++ extractie_func.js
@@ -178,7 +178,7 @@ define(function(){
             const doc = parser.parseFromString(xml_, "text/xml");
             //const results = document.getElementsByClassName('results');
             //let newtxt = '<table  class="docTable">';
-            newtxt += shownode(doc.childNodes[0]);
+           // newtxt += shownode(doc.childNodes[0]);
 
             // if (results)
 
@@ -188,7 +188,7 @@ define(function(){
 
             // }
 
-            console.log(newtxt);
+            console.log(shownode(doc.childNodes[0]));
 
         }
 
