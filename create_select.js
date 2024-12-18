add1a027c9e5a1a9807fcc44f0b8ca4bdf671acd
YassinRian <yassinrian@hotmail.com>
2024-12-18 13:12:30 +0100

    Commit made on 2024-12-18 13:12:30

--- create_select.js
+++ create_select.js
@@ -1,13 +1,22 @@
-define(['jquery'], function ($) {
+define(['jquery', 'https://yassinrian.github.io/extractie_func.js'], function ($, xml_funcs) {
 
     "use strict";
 
     function BasicControl () { };
 
-    BasicControl.prototype.draw = function(oPage) {
-      //let elm = oControlHost.container;
-      //$(elm).append("<h1>Hallo Yassin Rian!</h1>")
-      console.log(oPage.page.application.document.reportXML);
+    BasicControl.prototype.initialize = function(oPage, fnDoneInitializing){
+      this.xml_data = oPage.page.application.document.reportXML;
+      fnDoneInitializing();
+    }
+
+    BasicControl.prototype.draw = function(oControlHost) {
+      let elm = oControlHost.container;
+      $(elm).append("<button id='yassin_button'>Haal Velden op</button>");
+
+      $('#yassin_button').on('click', function(){
+        xml_funcs.verwerken(this.xml_data);
+      })
+      
     }
 
     return BasicControl;
