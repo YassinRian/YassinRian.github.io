867167e6b4455e6180b4d1d7db1426830b811532
YassinRian <yassinrian@hotmail.com>
2024-12-18 13:34:05 +0100

    Commit made on 2024-12-18 13:34:05

--- create_select.js
+++ create_select.js
@@ -12,6 +12,7 @@ define(['jquery', 'https://yassinrian.github.io/extractie_func.js'], function ($
     BasicControl.prototype.draw = function(oControlHost) {
       let elm = oControlHost.container;
       $(elm).append("<button id='yassin_button'>Haal Velden op</button>");
+      $(elm).append("<div id='result'></div>")
 
       $('#yassin_button').on('click', function(){
         xml_funcs.verwerken(this.xml_data);
