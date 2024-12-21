1ccd2f9f1553ed83a0487bd1f49b0d0c442517d9
YassinRian <yassinrian@hotmail.com>
2024-12-21 13:40:09 +0100

    Commit made on 2024-12-21 13:40:09

--- solidjs/main.js
+++ solidjs/main.js
@@ -1,18 +1,37 @@
-define(["jquery", "https://esm.sh/preact", "https://esm.sh/htm"], function (
-  $,
-  preact,
-  htm
-) {
-  const { h, render } = preact; // Extract `h` and `render` from Preact
-  const { html } = htm; // Extract `html` from HTM
+define(["jquery"], function ($) {
 
   class Main {
-    draw(oControlHost) {
-      elm = oControlHost.container;
-      // Create your app
-      const App = () => html`<h1>Hello World!</h1>`;
-      render(h(App), elm);
-    }
-  } //einde class
+
+    async initialize() {
+        await this.loadDependencies();
+      }
+  
+      loadDependencies() {
+        
+        // Create a new script element
+        const script = document.createElement("script");
+        // Set the script type to module
+        script.type = "module";
+        // Define the module imports and functionality
+        script.textContent = `
+            import { h, render } from 'https://esm.sh/preact';
+            import htm from 'https://esm.sh/htm';
+
+            // Initialize htm with Preact
+            const html = htm.bind(h);
+
+            function App (props) {
+            return html`<h1>Hello ${props.name}!</h1>`;
+            }
+
+            render(html`<${App} name="World" />`, document.body);
+
+        `;
+        // Append the script to the body
+        $("body").append(script);
+      }
+
+}
+ //einde class
   return Main;
 }); //einde define
