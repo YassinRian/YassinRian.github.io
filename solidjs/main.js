5fbf6b4eff97acca456d554a495bcd516ef91cbc
YassinRian <yassinrian@hotmail.com>
2024-12-21 12:10:27 +0100

    Commit made on 2024-12-21 12:10:27

---
+++ solidjs/main.js
@@ -0,0 +1,37 @@
+define(["jquery"], function ($) {
+  class Main {
+    constructor() {
+      this.esm = null;
+      this.web = null;
+      this.html = null;
+    }
+
+    async initialize() {
+      await this.loadDependencies();
+    }
+
+    loadDependencies() {
+      require([
+        "https://esm.sh/solid-js@1.8.1",
+        "https://esm.sh/solid-js/web",
+        "https://esm.sh/solid-js@1.8.1/html",
+      ], (esm, web, html) => {
+        this.esm = esm;
+        this.web = web;
+        this.html = html;
+      });
+    }
+
+    draw(oControlHost) {
+      const App = () => {
+        const [count, setCount] = createSignal(0),
+          timer = setInterval(() => setCount(count() + 1), 1000);
+        onCleanup(() => clearInterval(timer));
+        return html`<div>${count}</div>`;
+      };
+
+      elm = oControlHost.container;
+      render(App, elm);
+    }
+  } //einde class
+}); //einde define
