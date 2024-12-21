2cec5ddec214777f090312bbd0782d13f24b13e6
YassinRian <yassinrian@hotmail.com>
2024-12-21 02:51:01 +0100

    Commit made on 2024-12-21 02:51:01

--- parsing/modalMarkup.js
+++ parsing/modalMarkup.js
@@ -14,7 +14,7 @@ define(function() {
         },      
         modal: () => {
             return `
-            <div id="table_modal" class="modal" style="display: none; position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0,0,0,0.5); z-index: 1000;">
+            <div id="table_modal" class="modal" style="display: none; position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(222, 222, 222, 0.5); z-index: 1000;">
                 <div class="modal-content" draggable="true">                    
                     <div style="display: flex; justify-content: space-between; align-items: center;">
                         <span class="close-modal" style="font-size: 24px; cursor: pointer; color: #666;">&times;</span>
