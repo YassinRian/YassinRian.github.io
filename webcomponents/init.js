b61e46c5c8068350eacee8de18ffd1f16253a9e9
YassinRian <yassinrian@hotmail.com>
2024-12-22 17:31:46 +0100

    Commit made on 2024-12-22 17:31:46

--- webcomponents/init.js
+++ webcomponents/init.js
@@ -1,26 +1,28 @@
-define(["jquery", "https://yassinrian.github.io/webcomponents/webcomp1.js"], function ($, shoelace) {
+define([
+  "jquery",
+  "https://yassinrian.github.io/webcomponents/webcomp1.js",
+], function ($, shoelace) {
   "use strict";
 
   class WebComponent {
-
     constructor() {
       this.init();
     }
-    
+
     async init() {
       await shoelace.loadShoelaceModule();
       console.log("WebComponent initialized");
     }
 
     draw(oControlHost) {
-        oControlHost.container.innerHTML = `
-            <sl-alert type="info" open>
-            <sl-icon slot="icon" name="info-circle"></sl-icon>
-            <sl-button slot="dismiss" type="text"></sl-button>
-            This is an alert!
-            </sl-alert>`;
-  }
-} // End of class
+      const elm = oControlHost.container;
+      $(elm).html(`
+        <sl-alert open>
+        <sl-icon slot="icon" name="info-circle"></sl-icon>
+        This is a standard alert. You can customize its content and even the icon.
+        </sl-alert>`);
+    } // End of draw
+  } // End of class
 
   return WebComponent;
 }); // End of define
