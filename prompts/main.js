6a1e18e11b00473361d7db80b79124ada5cd2ce7
YassinRian <yassinrian@hotmail.com>
2024-12-22 14:49:52 +0100

    Commit made on 2024-12-22 14:49:52

--- prompts/main.js
+++ prompts/main.js
@@ -1,6 +1,11 @@
 define(["jquery",'https://yassinrian.github.io/prompts/index.js'], function ($, App) {
   function AppPrompts() {}
 
+  AppPrompts.prototype.initialize = function (oControlHost) {
+    console.log(oControlHost.page.getControlByName("prompt_control").instance)
+  
+  };
+
   AppPrompts.prototype.draw = function (oControlHost) {
     let elm = oControlHost.container;
     
