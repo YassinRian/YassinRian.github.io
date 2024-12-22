6824b379b3972f1e51cf4b283bd47bc32132c0a5
YassinRian <yassinrian@hotmail.com>
2024-12-22 18:11:40 +0100

    Commit made on 2024-12-22 18:11:40

--- webcomponents/webcomp1.js
+++ webcomponents/webcomp1.js
@@ -4,12 +4,12 @@ define(["jquery"], function ($) {
     return {
       loadShoelaceModule: async function () {
         try {
-          // Add the stylesheet dynamically
-          const link = document.createElement("link");
-          link.rel = "stylesheet";
-          link.href = "https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.19.1/cdn/themes/light.css";
-          document.head.appendChild(link);
-          console.log("Shoelace stylesheet added successfully");
+          // // Add the stylesheet dynamically
+          // const link = document.createElement("link");
+          // link.rel = "stylesheet";
+          // link.href = "https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.19.1/cdn/themes/light.css";
+          // document.head.appendChild(link);
+          // console.log("Shoelace stylesheet added successfully");
   
           // Load the Shoelace module
           const shoelaceModule = await import("https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.19.1/cdn/components/alert/alert.js");
