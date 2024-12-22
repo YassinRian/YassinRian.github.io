2a322ac3339f62edeb5d0caf2857d3832c0dfdbb
YassinRian <yassinrian@hotmail.com>
2024-12-22 17:41:34 +0100

    Commit made on 2024-12-22 17:41:34

--- webcomponents/init.js
+++ webcomponents/init.js
@@ -17,10 +17,46 @@ define([
     draw(oControlHost) {
       const elm = oControlHost.container;
       $(elm).html(`
-        <sl-alert open>
-        <sl-icon slot="icon" name="info-circle"></sl-icon>
-        This is a standard alert. You can customize its content and even the icon.
-        </sl-alert>`);
+        <sl-alert variant="primary" open>
+  <sl-icon slot="icon" name="info-circle"></sl-icon>
+  <strong>This is super informative</strong><br />
+  You can tell by how pretty the alert is.
+</sl-alert>
+
+<br />
+
+<sl-alert variant="success" open>
+  <sl-icon slot="icon" name="check2-circle"></sl-icon>
+  <strong>Your changes have been saved</strong><br />
+  You can safely exit the app now.
+</sl-alert>
+
+<br />
+
+<sl-alert variant="neutral" open>
+  <sl-icon slot="icon" name="gear"></sl-icon>
+  <strong>Your settings have been updated</strong><br />
+  Settings will take effect on next login.
+</sl-alert>
+
+<br />
+
+<sl-alert variant="warning" open>
+  <sl-icon slot="icon" name="exclamation-triangle"></sl-icon>
+  <strong>Your session has ended</strong><br />
+  Please login again to continue.
+</sl-alert>
+
+<br />
+
+<sl-alert variant="danger" open>
+  <sl-icon slot="icon" name="exclamation-octagon"></sl-icon>
+  <strong>Your account has been deleted</strong><br />
+  We're very sorry to see you go!
+</sl-alert>
+
+        
+        `);
     } // End of draw
   } // End of class
 
