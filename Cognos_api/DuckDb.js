dc3bd4065bebf1f5353eed0c845310e6966c2ea4
YassinRian <yassinrian@hotmail.com>
2026-04-22 15:39:21 +0200

    Commit made on 2026-04-22 15:39:21

---
+++ Cognos_api/DuckDb.js
@@ -0,0 +1,21 @@
+define(["jquery"], function($){
+    "use strict"
+
+    function MyControl() {}
+
+    MyControl.prototype.draw = function(oControlHost) {
+        const container = oControlHost.container;
+
+        const workerPath = "../ibmcognos/bi/js/dashboard-analytics/lib/@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js";
+
+        try {
+            const testWorker = new Worker(workerPath);
+            testWorker.onmessage = (e) => console.log("DuckDB Worker says:", e.data);
+            $("#duck-status").html("<b style='color:green;'>DuckDB Worker Initialized!</b>")
+        } catch(e) {
+            $("#duck-status").html("<b style='color:green;'>Security blocked Worker</b>")
+            console.error("Worker error:", e);
+        }
+    };
+    return MyControl;
+});
