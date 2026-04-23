01c24682da706515fe6549511f140dfbb8470ccb
YassinRian <yassinrian@hotmail.com>
2026-04-23 23:14:59 +0200

    Commit made on 2026-04-23 23:14:59

--- Cognos_api/CashflowView.js
+++ Cognos_api/CashflowView.js
@@ -8,40 +8,7 @@ define(["jquery"], function($) {
         }
 
         renderLayout() {
-            const style = `
-                <style>
-                    .rtm-dash-wrapper { 
-                        border: 3px solid #004699; 
-                        background: #fff; 
-                        border-radius: 4px; 
-                        min-height: 500px;
-                    }
-                    .rtm-toolbar { 
-                        background: #004699; 
-                        color: white; 
-                        padding: 12px; 
-                        font-family: sans-serif;
-                    }
-                    .rtm-chart-canvas { 
-                        width: 100%; 
-                        height: 500px; 
-                        background: #f9f9f9;
-                    }
-                    .rtm-status { padding: 10px; color: #666; font-size: 12px; }
-                </style>
-            `;
-
-            const layout = `
-                <div class="rtm-dash-wrapper">
-                    <div class="rtm-toolbar">Project Cashflow - Rotterdam</div>
-                    <div id="rtm-status-msg" class="rtm-status">Wachten op data...</div>
-                    <div id="${this.chartId}" class="rtm-chart-canvas"></div>
-                </div>
-            `;
-
-            this.$container.html(`
-                <h1>HALLOOOOO</h1>
-                `);
+            this.$container.html(`<h1>HALLOOOOO</h1>`);
         }
 
         getChartNode() {
