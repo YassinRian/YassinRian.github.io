d225c97769357680a03f414c7d78d92fac1002b3
YassinRian <yassinrian@hotmail.com>
2026-04-24 20:34:16 +0200

    Commit made on 2026-04-24 20:34:16

--- Cognos_api/Main.js
+++ Cognos_api/Main.js
@@ -6,26 +6,34 @@ define([
     class CashflowController {
         constructor() {
             this.view = null;
-            console.log("Controller: Loaded");
+            this.pendingData = null;
         }
 
         draw(oControlHost) {
-            console.log("Controller: draw()");
             this.view = new CashflowView(oControlHost);
             this.view.renderLayout();
+
+            if (this.pendingData) {
+                this.setData(oControlHost, this.pendingData);
+            }
         }
 
         setData(oControlHost, oData) {
-            console.log("Controller: setData for", oData.name);
-            
-            // Minimalist row check
-            const rowCount = (oData.rows) ? oData.rows.length : 0;
-            console.log("Controller: Rows found ->", rowCount);
+            if (oData.name === "store_cashflow") {
+                if (!this.view) {
+                    this.pendingData = oData;
+                    return;
+                }
 
-            if (this.view) {
-                this.view.updateStatus(rowCount > 0 ? "Data OK: " + rowCount : "Check Slots!");
-            } else {
-                console.warn("Controller: setData ran before draw!");
+                const rows = oData.rows || [];
+                if (rows.length > 0) {
+                    this.view.updateStatus("Data geladen: " + rows.length + " rijen.");
+                    this.pendingData = null;
+                    
+                    // Logic for DuckDB/ECharts initialization goes here next
+                } else {
+                    this.view.updateStatus("Wachten op data...");
+                }
             }
         }
     }
